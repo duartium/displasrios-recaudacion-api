1e54c2a8d747b685c3d3dd8181f9580c75506d1c
Byron Andres Duarte Morejon <is-bduarte@bancoguayaquil.com>
2021-11-16 16:12:09 -0500

    Se agrega swagger

--- Displasrios.Recaudacion.WebApi/Startup.cs
+++ Displasrios.Recaudacion.WebApi/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
 
 namespace Displasrios.Recaudacion.WebApi
 {
@@ -25,6 +27,22 @@ namespace Displasrios.Recaudacion.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = $"NEO {groupName}",
+                    Version = groupName,
+                    Description = "API GESTIÓN DE RECAUDACIÓN Y VENTAS",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "NEO",
+                        Email = "neutrinodevs.com",
+                        Url = new Uri("http://neutrinodevs.com/"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +52,10 @@ namespace Displasrios.Recaudacion.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(setup => {
+                setup.SwaggerEndpoint("/swagger/v1/swagger.json", "NEO API");
+            });
 
             app.UseRouting();
 
