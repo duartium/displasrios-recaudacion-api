f93ae22364121e44fe3ef9cd60d2a79b926dcf24
Byron Duarte <byronduarte95@gmail.com>
2022-08-25 23:25:19 -0500

    avance en endpoint para reporte de ingresos por vendedor

---
+++ Displasrios.Recaudacion.WebApi/Controllers/ReportController.cs
@@ -0,0 +1,50 @@
+﻿using Displasrios.Recaudacion.Core.Contracts.Repositories;
+using Displasrios.Recaudacion.Core.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Displasrios.Recaudacion.WebApi.Controllers
+{
+    [Route("api/v{version:apiVersion}/catalogs")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Authorize]
+    public class ReportController : BaseApiController<ReportController>
+    {
+        private readonly ISaleRepository _rpsSales;
+
+        public ReportController(ISaleRepository saleRepository)
+        {
+            _rpsSales = saleRepository;
+        }
+
+
+        /// <summary>
+        /// Obtiene los ingresos por vendedor
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetProviders()
+        {
+            var response = new Response<IEnumerable<string>>(true, "OK");
+
+            try
+            {
+                response.Data = _rpsProvider.GetAll(); ;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+                return Conflict(response.Update(false, ex.Message, null));
+            }
+        }
+
+    }
+}
